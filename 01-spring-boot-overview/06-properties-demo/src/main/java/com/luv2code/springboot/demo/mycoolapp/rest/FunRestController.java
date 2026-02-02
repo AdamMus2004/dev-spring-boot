7c7371925d7d24b82d61f0f822adfeb78b5fddb5
AdamMus2004 <adammuspraca@gmail.com>
2026-02-02 22:19:58 +0100

    feat: Add 06-properties-demo - for testing application properties

---
+++ 01-spring-boot-overview/06-properties-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,37 @@
+package com.luv2code.springboot.demo.mycoolapp.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @Value("${spring.application.name}")
+    private String name;
+
+    @GetMapping("/")
+    public String sayHello(){
+        return name;
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkOut(){
+        return "Run a hard 5k!";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day.";
+    }
+
+    @GetMapping("teaminfo")
+    public String getTeamInfo(){
+    return "Coach: "+ coachName+ ", team: "+teamName;
+    }
+}
