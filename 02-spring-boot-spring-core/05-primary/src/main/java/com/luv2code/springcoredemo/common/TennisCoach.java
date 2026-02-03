48f4a3ef11bda86afd3d3b55acd32535497b97cd
AdamMus2004 <adammuspraca@gmail.com>
2026-02-03 18:07:22 +0100

    feat: Add 05-primary - for testing primary

---
+++ 02-spring-boot-spring-core/05-primary/src/main/java/com/luv2code/springcoredemo/common/TennisCoach.java
@@ -0,0 +1,13 @@
+package com.luv2code.springcoredemo.common;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practise forehand for 15 minutes and backhand for 10 minutes.";
+    }
+}
