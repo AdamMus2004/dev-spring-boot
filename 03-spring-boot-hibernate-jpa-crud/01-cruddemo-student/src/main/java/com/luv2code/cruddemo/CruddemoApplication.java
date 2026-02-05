01ea8a1fa7925f49255daa43ae54bdb7c3696a22
AdamMus2004 <adammuspraca@gmail.com>
2026-02-05 22:20:06 +0100

    feat: Add StudentDAO interface StudentDAOImpl class and create createStudent method at Application class, and succesfully save first object in MySQL database

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.luv2code.cruddemo;
 
+import com.luv2code.cruddemo.dao.StudentDAO;
+import com.luv2code.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +15,19 @@ public class CruddemoApplication {
 	}
 
     @Bean
-    public CommandLineRunner commandLineRunner() {
+    public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
         return runner -> {
-            System.out.println("Hello world");
+            createStudent(studentDAO);
         };
+    }
+
+    private void createStudent(StudentDAO studentDAO) {
+        System.out.println("Creating new student object ...");
+        Student tmpStudent = new Student("Adam","Mus","adam@mus.pl");
+
+        System.out.println("Saving the student ...");
+        studentDAO.save(tmpStudent);
+
+        System.out.println("Saved student. Generated id: " + tmpStudent.getId());
     }
 }
