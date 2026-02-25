a58b500148cd185861fa0a541818a5886733c5cd
AdamMus2004 <adammuspraca@gmail.com>
2026-02-25 17:48:25 +0100

    feat: Create mapping for patch method

--- 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -2,17 +2,23 @@ package com.luv2code.springboot.cruddemo.rest;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import tools.jackson.databind.json.JsonMapper;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
 
     private EmployeeService employeeService;
+    private JsonMapper jsonMapper;
 
-    public EmployeeRestController(EmployeeService employeeService){
+    @Autowired
+    public EmployeeRestController(EmployeeService employeeService,JsonMapper jsonMapper){
+        this.jsonMapper=jsonMapper;
         this.employeeService = employeeService;
     }
 
@@ -25,7 +31,7 @@ public class EmployeeRestController {
     public Employee getEmployeeById(@PathVariable int id) {
         Employee employee = employeeService.findById(id);
         if (employee==null) {
-            throw new RuntimeException("Employee id not found id: ["+id+"]");
+            throw new RuntimeException("Employee id not found - "+id);
         } else {
             return employee;
         }
@@ -47,5 +53,21 @@ public class EmployeeRestController {
     public Employee updateEmployee(@RequestBody Employee employee) {
        Employee dbEmployee = employeeService.save(employee);
        return dbEmployee;
+    }
+    @PatchMapping("/employees/{id}")
+    public Employee patchEmployee(@PathVariable int id, @RequestBody Map<String, Object> patchPayload) {
+        Employee tempEmployee = employeeService.findById(id);
+
+        if (tempEmployee == null) {
+            throw new RuntimeException("Employee id not found - "+id);
+        }
+
+        if (patchPayload.containsKey("id")) {
+            throw new RuntimeException("Employee id not allowed in request body - "+id);
+        }
+
+        Employee patchedEmployee = jsonMapper.updateValue(tempEmployee,patchPayload);
+        Employee dbEmployee =  employeeService.save(patchedEmployee);
+        return dbEmployee;
     }
 }
