b873fcf0a867880f16daf0ab51e782284ff19551
AdamMus2004 <adammuspraca@gmail.com>
2026-02-25 15:51:09 +0100

    feat: Create all CRUD methods in EmployeeDAOJpaImpl

--- 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
+++ 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -25,4 +25,28 @@ public class EmployeeDAOJpaImpl implements EmployeeDAO{
 
         return employees;
     }
+
+    @Override
+    public Employee findById(int id) {
+
+        Employee employee = entityManager.find(Employee.class,id);
+
+        return employee;
+    }
+
+    @Override
+    public Employee save(Employee employee) {
+
+        Employee dbEmployee = entityManager.merge(employee);
+        return dbEmployee;
+    }
+
+    @Override
+    public void deleteById(int id) {
+        Employee employeeToDelete = entityManager.find(Employee.class,id);
+
+        entityManager.remove(employeeToDelete);
+    }
+
+
 }
