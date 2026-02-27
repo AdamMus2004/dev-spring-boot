edb582b6d0ad69dc839a65135444ea4fb31cd487
Adam Mus <adammuspraca@gmail.com>
2026-02-27 23:57:33 +0100

    feat: Change @RepositoryRestResource

--- 04-spring-boot-rest-crud/40-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/40-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -4,6 +4,6 @@ import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "members")
+@RepositoryRestResource(path = "people")
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 }
