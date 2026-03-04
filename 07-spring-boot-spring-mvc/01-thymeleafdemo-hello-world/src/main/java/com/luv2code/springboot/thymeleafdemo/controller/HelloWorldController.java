b9ebedb1e90fdff6106715221d4f0e6709d767cd
AdamMus2004 <adammuspraca@gmail.com>
2026-03-04 16:01:21 +0100

    feat: Add letsShout method in HelloWorldCOntroller

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-hello-world/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-hello-world/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,6 +14,17 @@ public class HelloWorldController {
 
     @RequestMapping("/processForm")
     public String processForm() {
+        return "helloworld";
+    }
+    @RequestMapping("/processFormVersionTwo")
+    public String lestShoutDude(HttpServletRequest request, Model model) {
+        String theName = request.getParameter("studentName");
+
+        theName = theName.toUpperCase();
+
+        String message = "Yo! "+ theName;
+
+        model.addAttribute("message",message);
         return "helloworld";
     }
 }
