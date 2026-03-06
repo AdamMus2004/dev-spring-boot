89f97b22bc160658c74e23f137580bbb33ee20ed
Adam Mus <adammuspraca@gmail.com>
2026-03-06 15:38:00 +0100

    feat: Add @NotNull, @Min, @Max validation and update html files

--- 07-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ 07-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -1,14 +1,26 @@
 package com.luv2code.springdemo.mvc;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
     private String firstName;
-
+    @NotBlank(message = "Can't be blank")
     @NotNull(message = "is required")
+
     @Size(min = 1,message = "is required")
     private String lastName;
+
+    @Min(value = 0, message = "must be grater than or equal to zero")
+    @Max(value = 10, message = "must be less than or equal to ten")
+    private int freePasses;
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
+    }
 
     public String getFirstName() {
         return firstName;
