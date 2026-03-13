2124b4ddbf7a334225d57cd43c0260e1e751c537
Adam Mus <adammuspraca@gmail.com>
2026-03-13 18:57:22 +0100

    feat: Add SecurityFilterChain method and LoginController class

--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -2,12 +2,15 @@ package com.luv2code.springboot.demosecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
+
     @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
         UserDetails john = User.builder()
@@ -18,16 +21,31 @@ public class DemoSecurityConfig {
 
         UserDetails mary = User.builder()
                 .username("mary")
-                .password("test123")
+                .password("{noop}test123")
                 .roles("EMPLOYEES","MANAGER")
                 .build();
 
         UserDetails susan = User.builder()
                 .username("susan")
-                .password("test123")
+                .password("{noop}test123")
                 .roles("EMPLOYEES","MANAGER","ADMIN")
                 .build();
 
         return new InMemoryUserDetailsManager(john,mary,susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .anyRequest().authenticated()
+        )
+                .formLogin(form ->
+                        form
+                                .loginPage("/showMyLoginPage")
+                                .loginProcessingUrl("/authenticateTheUser")
+                                .permitAll()
+                );
+                return  http.build();
     }
 }
