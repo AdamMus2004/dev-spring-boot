42f3cea447a0bdeffc037e7cbec089860c713974
Adam Mus <adammuspraca@gmail.com>
2026-04-09 14:06:01 +0200

    feat: Crate 4 @Ascpect class

--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -18,15 +18,19 @@ public class MyDemoLoggingAspect {
     @Pointcut("execution(* com.luv2code.aopdemo.dao.*.set*())")
     private void setter() {}
 
+    @Pointcut("forDaoPackage() && !(getter() || setter())")
+    private void forDaoPackageWithoutGetterAndSetters() {}
 
 
-    @Before("forDaoPackage()")
+    @Before("forDaoPackageWithoutGetterAndSetters()")
     public void beforeAddAccountAdvice() {
-        System.out.println("\n======>>>> Executing @Before advice on addAccount()");
-    }
+        System.out.println("\n======>>>> Executing @Before advice on addAccount()");}
 
-    @Before("forDaoPackage()")
+    @Before("forDaoPackageWithoutGetterAndSetters()")
     public void performApiAnalytics() {
-        System.out.println("\n======>>>> Performing API analytics");
-    }
+        System.out.println("\n======>>>> Performing API analytics");}
+
+    @Before("forDaoPackageWithoutGetterAndSetters()")
+    public void logToCloudAsync() {
+        System.out.println("\n======>>>> Logging to Cloud in async fashion");}
 }
